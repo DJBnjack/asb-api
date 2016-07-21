8a58536f4b7c1ed33e94f31cbcf9f65372497a1a
Johannes Bertens <johannes.bertens@live.nl>
2016-07-21 16:29:14 +0200

    Why is it not working??

--- server.js
+++ server.js
@@ -1,42 +1,36 @@
-// 'use strict';
+'use strict';
 
-// var port = process.env.PORT || 8000;
-// var env = process.env.APPSETTING_env || "local";
-// var queue = process.env.APPSETTING_queue || "asb-queue-dev";
-// var release = process.env.APPSETTING_release || "undefined";
-
-// var http = require('http');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var swaggerize = require('swaggerize-express');
-// var swaggerUi = require('swaggerize-ui');
-// var path = require('path');
-
-// var app = express();
-
-// var server = http.createServer(app);
-
-// app.use(bodyParser.json());
-
-// app.use(swaggerize({
-//   api: path.resolve('./config/api.json'),
-//   handlers: path.resolve('./handlers'),
-//   docspath: '/swagger/docs/v1'
-// }));
-
-// app.use('/swagger', swaggerUi({
-//   docs: '/swagger/docs/v1',
-//   port: port
-// }));
-
-// app.get('/', function(req, res){
-//   res.send('Hello! This API is running: <br>Release: ' + release + '<br>Environment: ' + env + '<br>Queue: ' + queue + '<br><a href="/swagger">Swagger</a>');
-// });
-
-// server.listen(port, function () {});
+var port = process.env.PORT || 8000;
+var env = process.env.APPSETTING_env || "local";
+var queue = process.env.APPSETTING_queue || "asb-queue-dev";
+var release = process.env.APPSETTING_release || "undefined";
 
 var http = require('http');
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello from Azure running node version: ' + process.version + '</br>');
-}).listen(process.env.PORT || 3000);
+var express = require('express');
+var bodyParser = require('body-parser');
+var swaggerize = require('swaggerize-express');
+var swaggerUi = require('swaggerize-ui');
+var path = require('path');
+
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(bodyParser.json());
+
+app.use(swaggerize({
+  api: path.resolve('./config/api.json'),
+  handlers: path.resolve('./handlers'),
+  docspath: '/swagger/docs/v1'
+}));
+
+app.use('/swagger', swaggerUi({
+  docs: '/swagger/docs/v1',
+  port: port
+}));
+
+app.get('/', function(req, res){
+  res.send('Hello! This API is running: <br>Release: ' + release + '<br>Environment: ' + env + '<br>Queue: ' + queue + '<br><a href="/swagger">Swagger</a>');
+});
+
+server.listen(port);
